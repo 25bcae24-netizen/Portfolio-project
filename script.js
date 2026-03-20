880d5b6334015e29ad3389630d776c359fe918eb
Harshita R <25bcae24@kristujayanti.com>
2026-03-20 18:35:53 +0530

    Added script.js and connected frontend to backend

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector("form");
 
@@ -28,3 +29,31 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+=======
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const data = {
+    name: form.querySelector("[name='name']").value,
+    email: form.querySelector("[name='email']").value,
+    message: form.querySelector("[name='message']").value
+  };
+
+  fetch("https://harshita-r-backend.onrender.com/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(() => {
+    alert("Message sent successfully!");
+    form.reset();
+  })
+  .catch(() => {
+    alert("Error sending message");
+  });
+});
+>>>>>>> 4d7928f (Added script.js and connected frontend to backend)
