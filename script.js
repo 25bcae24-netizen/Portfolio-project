91a912de20fb9c34ac7a1c47bdd38fe441330e1b
Harshita R <25bcae24@kristujayanti.com>
2026-03-23 21:54:22 +0530

    fix DOM load issue

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-document.querySelector(".contact-form").addEventListener("submit", function(e){
-  e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
 
-  fetch("https://harshita-r-backend.onrender.com/contact", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      name: document.querySelector("[name='name']").value,
-      email: document.querySelector("[name='email']").value,
-      message: document.querySelector("[name='message']").value
+  document.querySelector(".contact-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    alert("Form submitted"); // test
+
+    fetch("https://harshita-r-backend.onrender.com/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: document.querySelector("[name='name']").value,
+        email: document.querySelector("[name='email']").value,
+        message: document.querySelector("[name='message']").value
+      })
     })
-  })
-  .then(res => {
-    console.log(res);
-    return res.text();
-  })
-  .then(data => {
-    alert("Response: " + data);
-  })
-  .catch(err => {
-    alert("Error connecting to backend");
-    console.log(err);
+    .then(res => res.text())
+    .then(data => alert(data))
+    .catch(err => {
+      alert("Error connecting to backend");
+      console.log(err);
+    });
+
   });
+
 });
