61b1993117899aec3cc97d4fbe0ce19c55ae953a
Harshita R <25bcae24@kristujayanti.com>
2026-03-23 23:02:12 +0530

    fix render crash

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,6 @@ app.use(cors({
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"]
 }));
-app.options("*",cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
